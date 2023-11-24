23ee5b01f4f9c6abacad0d69643896b130fde15b
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-24 16:59:39 +0100

    Update player.py

---
+++ src/player.py
@@ -0,0 +1,10 @@
+# player.py
+
+class Player:
+    def __init__(self):
+        # Initialize player attributes here
+        pass
+
+    def move(self, direction):
+        # Player movement logic
+        pass
