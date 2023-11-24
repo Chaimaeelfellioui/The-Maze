9524c03bdd336ab00766326ef3bf8a95c104e9c0
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-24 17:03:39 +0100

    Update ui.py

---
+++ src/ui.py
@@ -0,0 +1,10 @@
+# ui.py
+
+class UI:
+    def __init__(self):
+        # Initialize UI components
+        pass
+
+    def render_maze(self, maze):
+        # Display the maze on the screen
+        pass
