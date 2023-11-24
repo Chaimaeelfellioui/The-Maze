566cbb04fad8e4b315fbd6b6d632358e6fabd634
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-24 17:31:46 +0100

    Update player.py

--- src/player.py
+++ src/player.py
@@ -8,3 +8,17 @@ class Player:
     def move(self, direction):
         # Player movement logic
         pass
+class Player:
+    def __init__(self, start_row, start_col):
+        self.row = start_row
+        self.col = start_col
+
+    def move(self, direction):
+        if direction == "UP":
+            self.row -= 1
+        elif direction == "DOWN":
+            self.row += 1
+        elif direction == "LEFT":
+            self.col -= 1
+        elif direction == "RIGHT":
+            self.col += 1
