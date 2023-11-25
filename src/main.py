80aee8e365ccd625ac07056e413577ba9788d632
Chaimae El Fellioui <125446488+Chaimaeelfellioui@users.noreply.github.com>
2023-11-25 16:29:15 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,9 @@
 from game import Game
 
 def main():
-    game = Game()
+    maze_width = 10  # Replace with the desired width of the maze
+    maze_height = 10  # Replace with the desired height of the maze
+    game = Game(maze_width, maze_height)
     game.run()
 
 if __name__ == "__main__":
