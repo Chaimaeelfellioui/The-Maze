165355e42889d025058b692b01e7c57bc41aa09e
Chaimaeelfellioui <chaimae1307@gmail.com>
2023-11-25 10:06:22 -0800

    Finalizing the game

---
+++ tests/test_game.py
@@ -0,0 +1,14 @@
+import unittest
+from game import Game
+
+class TestGame(unittest.TestCase):
+    def test_initialization(self):
+        # Your tests here
+        pass
+
+    def test_game_run(self):
+        # Your tests here
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
