b47b2a08aaa5a556d44532e4fa4f715bbaf93598
Chaimaeelfellioui <chaimae1307@gmail.com>
2023-11-25 10:03:18 -0800

    task

--- src/game.py
+++ src/game.py
@@ -4,24 +4,22 @@ from player import Player
 from maze_generation import generate_maze
 from collision import check_collision
 
+
 class Game:
     def __init__(self, maze_width, maze_height):
         self.maze = generate_maze(maze_width, maze_height)
-        start_row, start_col = 0, 0  # Choose the initial position of the player
+        start_row, start_col = 0, 0
         self.player = Player(start_row, start_col)
 
     def run(self):
         while True:
-            # Update the player's state based on user inputs
             direction = input("Enter the direction (UP/DOWN/LEFT/RIGHT): ")
             self.player.move(direction)
 
-            # Check for collision
             if check_collision(self.player, self.maze):
                 print("Collision detected. The game is over.")
                 break
 
-            # Display the maze with the current player's position
             self.display_game_state()
 
             # Add a win condition here
@@ -29,13 +27,14 @@ class Game:
                 print("Congratulations! You won!")
                 break
 
+            # Add additional conditions to end the game if needed
+
     def display_game_state(self):
-        # Display logic for the maze with the player's position
         for row in self.maze:
             print(" ".join("X" if cell == 1 else " " for cell in row))
         print("Player position: ({}, {})".format(self.player.row, self.player.col))
 
     def player_has_won(self):
-        # Implement win condition logic here
-        # For example, check if the player has reached a goal
-        return False  # Replace this with your win condition logic
+        # Add logic to check if the player has won
+        # For example, if the player reaches a specific position
+        return False  # Replace with your win condition
